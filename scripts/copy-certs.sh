24d871a928506544cf91497a0bbbafec95441119
gq97a6 <sz.kg@protonmail.com>
2024-10-01 17:50:23 +0200

    Docker compose fix.

--- scripts/copy-certs.sh
+++ scripts/copy-certs.sh
@@ -1,7 +1,7 @@
-cp /etc/letsencrypt/live/hostunit.net/fullchain.pem /own/docker/storage/nginx/certs/hostunit.net/fullchain.pem
-cp /etc/letsencrypt/live/hostunit.net/privkey.pem /own/docker/storage/nginx/certs/hostunit.net/privkey.pem
+cp /etc/letsencrypt/live/hostunit.net/fullchain.pem /own/docker/storage/nginx/certs/hostunit.net/
+cp /etc/letsencrypt/live/hostunit.net/privkey.pem /own/docker/storage/nginx/certs/hostunit.net/
 
-cp /etc/letsencrypt/live/alteratom.com/fullchain.pem /own/docker/storage/nginx/certs/alteratom.com/fullchain.pem
-cp /etc/letsencrypt/live/alteratom.com/privkey.pem /own/docker/storage/nginx/certs/alteratom.com/privkey.pem
+cp /etc/letsencrypt/live/alteratom.com/fullchain.pem /own/docker/storage/nginx/certs/alteratom.com/
+cp /etc/letsencrypt/live/alteratom.com/privkey.pem /own/docker/storage/nginx/certs/alteratom.com/
 
 echo "Copied!"
