aa385daff13320b646e61057b5e5c6b363f1a46e
gq97a6 <sz.kg@protonmail.com>
2025-08-19 00:16:39 +0000

    NextChat replaced by LibreChat. Compose files split into seperete files. .env split into seperate files.

--- docker/scripts/copy-certs.sh
+++ docker/scripts/copy-certs.sh
@@ -1,8 +1,8 @@
 cd /own/docker/storage
 
-cp certbot/letsencrypt/live/hostunit.net/fullchain.pem   nginx/certs/hostunit.net/
-cp certbot/letsencrypt/live/hostunit.net/privkey.pem     nginx/certs/hostunit.net/
-cp certbot/letsencrypt/live/alteratom.com/fullchain.pem  nginx/certs/alteratom.com/
-cp certbot/letsencrypt/live/alteratom.com/privkey.pem    nginx/certs/alteratom.com/
+cp /etc/letsencrypt/live/hostunit.net/fullchain.pem   nginx/certs/hostunit.net/
+cp /etc/letsencrypt/live/hostunit.net/privkey.pem     nginx/certs/hostunit.net/
+cp /etc/letsencrypt/live/alteratom.com/fullchain.pem  nginx/certs/alteratom.com/
+cp /etc/letsencrypt/live/alteratom.com/privkey.pem    nginx/certs/alteratom.com/
 
 echo "Copied!"
